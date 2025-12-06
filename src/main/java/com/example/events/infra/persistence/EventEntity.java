19ae2bbe081b97f0fbda2449b77553210f8e1c23
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-06 11:32:57 -0300

    feat: create events repository

--- src/main/java/com/example/events/infra/persistence/EventEntity.java
+++ src/main/java/com/example/events/infra/persistence/EventEntity.java
@@ -1,79 +1,57 @@
 package com.example.events.infra.persistence;
 
 import com.example.events.core.enums.EventType;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "events")
 public class EventEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", nullable = false)
     private UUID id;
 
-    @Column(name = "sponsor_id")
+    @Column(name = "sponsor_id", nullable = false)
     private UUID sponsorId;
+
+    @Column(length = 64, unique = true, nullable = false)
     private String identifier;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private EventType type;
+
+    @Column(length = 300, nullable = false)
     private String name;
 
-    @Column(nullable = true)
+    @Column(length = 600)
     private String description;
 
-    @Column(name = "start_time")
-    private LocalDateTime startTime;
-
-    @Column(name = "end_time")
-    private LocalDateTime endTime;
-
+    @Column(nullable = false)
     private String location;
 
-    @Column(nullable = true)
     private Short capacity;
 
-    @Column(name = "created_at")
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = true)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    public EventEntity(
-           UUID id,
-           UUID sponsorId,
-           String identifier,
-           EventType type,
-           String name,
-           String description,
-           LocalDateTime startTime,
-           LocalDateTime endTime,
-           String location,
-           Short capacity,
-           LocalDateTime createdAt,
-           LocalDateTime updatedAt
-    ) {
-        this.id = id;
-        this.sponsorId = sponsorId;
-        this.identifier = identifier;
-        this.type = type;
-        this.name = name;
-        this.description = description;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.location = location;
-        this.capacity = capacity;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 }
