66b6359eca1274069b83fc980a4c49bfd1e783b4
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-10 10:49:29 -0300

    fix: add missing annotation @Repository into JPA

--- src/main/java/com/example/events/infra/persistence/EventsRepository.java
+++ src/main/java/com/example/events/infra/persistence/EventsRepository.java
@@ -1,10 +1,12 @@
 package com.example.events.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface EventsRepository extends JpaRepository<EventEntity, UUID> {
     List<EventEntity> findBySponsorId(UUID sponsorId);
 }
--- src/main/java/com/example/events/infra/persistence/UsersRepository.java
+++ src/main/java/com/example/events/infra/persistence/UsersRepository.java
@@ -1,8 +1,10 @@
 package com.example.events.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UsersRepository extends JpaRepository<UserEntity, UUID> {
 }
