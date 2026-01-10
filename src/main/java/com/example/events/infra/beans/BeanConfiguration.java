215ca2f0557891f50355f624d392bebd7fb318bf
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-10 11:23:47 -0300

    feat: add use case to create user  into singleton

--- src/main/java/com/example/events/infra/beans/BeanConfiguration.java
+++ src/main/java/com/example/events/infra/beans/BeanConfiguration.java
@@ -1,8 +1,11 @@
 package com.example.events.infra.beans;
 
 import com.example.events.core.gateway.EventGateway;
+import com.example.events.core.gateway.UserGateway;
 import com.example.events.core.usecases.CreateEventUseCase;
 import com.example.events.core.usecases.CreateEventUseCaseImpl;
+import com.example.events.core.usecases.CreateUserUseCase;
+import com.example.events.core.usecases.CreateUserUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,11 @@ public class BeanConfiguration {
     @Bean
     public CreateEventUseCase createEventUseCase(EventGateway eventGateway){
         return new CreateEventUseCaseImpl(eventGateway);
+    }
+
+    @Bean
+    public CreateUserUseCase createUserUseCase(UserGateway
+            userGateway){
+        return new CreateUserUseCaseImpl(userGateway);
     }
 }
